12d6aa1a0641c900d6c0297086fa874f1de1a0a7
Firliandy <lian.eddy@gmail.com>
2020-01-17 11:47:24 +0700

    keeplogin unfinished

--- controller/userController.js
+++ controller/userController.js
@@ -29,18 +29,30 @@ module.exports = {
         db.query(sql, (err, results) => {
             if(err) res.status(500).send(err);
 
-            let { id, username, password, email, address } = results[0]
-            // const token = createJWTToken({
-            //     id,
-            //     username    
-            // })
-            return res.status(200).send({
-                id,
-                username, 
-                password, 
-                // email,
-                // token
-            })
+            if(results && results.length >0){
+                let { id, username, password, email, address } = results[0]
+                const token = createJWTToken({
+                    id,
+                    username, 
+                    password,
+                    email,
+                    address    
+                })
+                console.log(token)
+                return res.status(200).send({
+                    id,
+                    username, 
+                    email, 
+                    address,
+                    // password, 
+                    // email,
+                    token
+                })
+            }else{
+                res.status(200).send('User or Password Invalid')
+            }
+
+            
         })
     },
     deleteUser : (req,res) => {
@@ -120,5 +132,29 @@ module.exports = {
                 }).status(200)
             }
         })
+    },
+    keepLogin : (req,res) => {
+        console.log(req.user);
+        let sql = `select * from users where id = ${req.user.id};`;
+        db.query(sql, (err,results) => {
+            if(err) res.status(500).send(err)
+
+            const { id, username, password, email, address } = results[0]
+            
+            const token = createJWTToken({
+                id,
+                username, 
+                password,
+                email,
+                address    
+            })
+            return res.status(200).send({
+                id,
+                username, 
+                email, 
+                address,
+                token
+            })
+        }) 
     }
 }
