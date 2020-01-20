7202ac6a1a0d56380ea1ff5aa891cc7826c14c21
Firliandy <lian.eddy@gmail.com>
2020-01-20 11:37:08 +0700

    uploader

--- controller/imageController.js
+++ controller/imageController.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 module.exports = {
     uploadImage : (req,res) => {
         console.log('uploader')
+        console.log(req.files)
         try{
             const path = '/images'
             const upload = uploader(path, 'IMG').fields([{ name : 'image' }])
@@ -16,15 +17,16 @@ module.exports = {
                 }
                 const {image} = req.files;
                 const imagePath = image ? path +'/' + image[0].filename : null
-                let data = {
-                    imagepath : imagePath
-                }
+
+                console.log(req.body.data)
+                const data = JSON.parse(req.body.data)
+                data.imagepath = imagePath
                 
                 let sql = 'insert into images set ?';
                 db.query(sql, data, (err,results) => {
                     if(err){
                         console.log(err)
-                        fs.unlinkSync('./public' + imagePath)
+                        fs.unlinkSync('./public' + imagePath) //kalo error delete file
                         return res.status(500).send({message : 'error'})
                     }
                     return res.status(200).send(results)
@@ -34,5 +36,15 @@ module.exports = {
         catch(err){
             return res.status(500).send({message: 'error'})
         }
+    },
+    getAllImages : (req,res) => {
+        console.log(req.query)
+        let sql = `SELECT * FROM images;`
+        db.query(sql, (err, results) => {
+            if(err){
+                res.status(500).send(err)
+            }
+            res.status(200).send(results)
+        });
     }
 }
--- router/imageRouter.js
+++ router/imageRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { imageController } = require('../controller');
 const router = express.Router();
 
-router.post('/upload', imageController.uploadImage)
+router.post('/upload', imageController.uploadImage);
+router.get('/getproducts', imageController.getAllImages)
 
 module.exports = router;
